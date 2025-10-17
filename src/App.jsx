6c2380c67a5182c6540e8a01ba2105bcf9891dcd
Alish-Shakya <alishshakya44@gmail.com>
2025-10-17 09:55:27 +0545

    initilization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <p className="text-4xl text-center text-green-500 p-20">
+        Hello world! welcome to my GMS project
+      </p>
+    </>
+  );
+}
+
+export default App;
